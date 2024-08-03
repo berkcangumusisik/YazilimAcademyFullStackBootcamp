77b5507982508c3d92dfa8b34709107df8408f82
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-08-03 12:21:01 +0300

    Password Storaged App Web Api Started.

---
+++ PasswordStorageApp/PasswordStorageApp.WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasswordStorageApp.WebApi.Persistence;
+
+namespace PasswordStorageApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+
+        [HttpGet] 
+        public IActionResult GetAll()
+        {
+            var accounts = FakdeDbContext.Accounts.ToList();
+            return Ok(accounts);
+        }
+
+        [HttpGet("{id:guid}")] 
+        public IActionResult GetById(Guid id)
+        {
+            var account = FakdeDbContext.Accounts.FirstOrDefault(ac => ac.Id == id);
+            if (account is null)
+            {
+                return NotFound();
+            }
+            return Ok(account);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult Remove(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return BadRequest("id is not valid. Please do not send empty guids for god sake!");
+            }
+
+            var account = FakdeDbContext.Accounts.FirstOrDefault(ac => ac.Id == id);
+            if (account is null)
+            {
+                return NotFound();
+            }
+            FakdeDbContext.Accounts.Remove(account);
+            return NoContent();
+        }
+    }
+}
